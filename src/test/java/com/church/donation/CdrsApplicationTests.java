397b05472022029cc0667284559fa1ab70faec00
황재욱 <ghkdwodnr123@gmail.com>
2026-05-07 14:58:32 +0900

    최초 시각화 + 폴더

---
+++ src/test/java/com/church/donation/CdrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.church.donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

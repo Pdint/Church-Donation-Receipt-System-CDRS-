faaf81dd56d1aa6f9abf0821a8463f5e1d860016
황재욱 <ghkdwodnr123@gmail.com>
2026-06-01 13:57:41 +0900

    설명문 조정

--- src/main/java/com/church/donation/CdrsApplication.java
+++ src/main/java/com/church/donation/CdrsApplication.java
@@ -3,6 +3,9 @@ package com.church.donation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing  // 이 스위치를 반드시 달아주어야 시간이 자동으로 들어갑니다!!
 @SpringBootApplication
 public class CdrsApplication {
     public static void main(String[] args) {
--- src/main/java/com/church/donation/domain/BaseTimeEntity.java
+++ src/main/java/com/church/donation/domain/BaseTimeEntity.java
@@ -4,15 +4,16 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
-@EntityListeners(AuditingEntityListener.class) // 엔티티가 영속성 컨텍스트에 적재될 때 시간을 자동으로 매핑
+//@EntityListeners(AuditingEntityListener.class) // 엔티티가 영속성 컨텍스트에 적재될 때 시간을 자동으로 매핑
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(updatable = false) // 👈 데이터가 등록된 이후에는 절대 수정(UPDATE)할 수 없도록 강제 제어
     private LocalDateTime createdAt;
 }
